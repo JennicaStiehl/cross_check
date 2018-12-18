f60ffadc2c7063f16ea28bb6553e66136f539a3c
earl-stephens <stephensearl@hotmail.com>
2018-12-18 16:54:43 -0700

    created a Team class, updated and changed Stat_tracker class to be able to import sample_team_info.csv

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,29 +1,35 @@
 require "csv"
 require 'pry'
+require './lib/team'
 class StatTracker
-  attr_reader
-              # :abbreviations
+  attr_reader :teams
 
   def initialize
-    # @locations = []
-    # @abbreviations = []
+    @teams = {}
   end
 
-  def self.from_csv(locations)
-    # binding.pry
-    abbreviations = []
-    lines = CSV.open locations, headers: true, header_converters: :symbol
-    lines.each do |line|
-      abbreviations.push(line[:abbreviation])
+  def parse_teams(file_path)
+  skip_first_line = true
+  CSV.foreach(file_path) do |row|
+    unless skip_first_line
+      # team = Team.new
+      @teams[row[0].to_i] = ([
+        # row[0].to_i =>
+        # [
+        row[1],
+        row[2],
+        row[3],
+        row[4],
+        row[5]])
+        # ]
+        # )
+        # binding.pry
+    else
+      skip_first_line = false
     end
-    abbreviations
-    #when pry opens in runner.rb, type stat_tracker
-    #and you'll see the abbreviations array
+
+  end
+@teams
   end
 
 end
-
-
-      # binding.pry
-      # @cards << Card.new(row[:question], row[:answer], row[:category].to_sym)
-    # end
--- runner.rb
+++ runner.rb
@@ -10,6 +10,6 @@ locations = {
   game_teams: game_teams_path
 }
 
-stat_tracker = StatTracker.from_csv(locations)
+stat_tracker = StatTracker.from_csv('./data/sample_team_info.csv')
 
 require 'pry'; binding.pry
