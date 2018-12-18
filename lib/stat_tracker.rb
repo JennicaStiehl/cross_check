d3107d417e44d976acc9fab866376591486447ae
earl-stephens <stephensearl@hotmail.com>
2018-12-18 08:51:40 -0700

    created sample data files and started stat_trecker tests

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,4 +1,29 @@
-CSV.foreach(@file_path, {headers: true, header_converters: :symbol}) do |row|
-      binding.pry
-      @cards << Card.new(row[:question], row[:answer], row[:category].to_sym)
+require "csv"
+require 'pry'
+class StatTracker
+  attr_reader
+              # :abbreviations
+
+  def initialize
+    # @locations = []
+    # @abbreviations = []
+  end
+
+  def self.from_csv(locations)
+    # binding.pry
+    abbreviations = []
+    lines = CSV.open locations, headers: true, header_converters: :symbol
+    lines.each do |line|
+      abbreviations.push(line[:abbreviation])
     end
+    abbreviations
+    #when pry opens in runner.rb, type stat_tracker
+    #and you'll see the abbreviations array
+  end
+
+end
+
+
+      # binding.pry
+      # @cards << Card.new(row[:question], row[:answer], row[:category].to_sym)
+    # end
