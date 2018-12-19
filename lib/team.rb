b8d05e9c2fa21563980f75961871e9efca575f1a
JennicaStiehl <jennica.stiehl@gmail.com>
2018-12-18 17:31:02 -0700

    adds team test

--- lib/team.rb
+++ lib/team.rb
@@ -3,9 +3,9 @@ class Team
   def initialize
   end
 
-  def add_team(attributes)
-    team_hash = attributes
-    team_hash
-  end
+  # def add_team(attributes)
+  #   team_hash = attributes
+  #   team_hash
+  # end
 
 end
--- test/team_test.rb
+++ test/team_test.rb
@@ -1,1 +1,12 @@
-require 'minitest/autorun '
+require './test/test_helper'
+require './lib/team'
+
+class TeamTest < Minitest::Test
+
+  def test_it_exists
+    team = Team.new
+    assert_instance_of Team, team
+  end
+
+
+end
