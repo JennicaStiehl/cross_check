7d53e1f9ec783f63ae241b1518e2971be0cec80e
Erin King <eohone@gmail.com>
2018-12-19 08:21:41 -0700

    adds test_it_works_for_sample_game_info_file

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -58,4 +58,31 @@ class StatTracker
     end
     @games
   end
+
+  def parse_game_teams(file_path)
+  skip_first_line = true
+  CSV.foreach(file_path) do |row|
+      unless skip_first_line
+        @games[row[0].to_i] = ([
+          row[1],
+          row[2],
+          row[3],
+          row[4],
+          row[5],
+          row[6],
+          row[7],
+          row[8],
+          row[9],
+          row[10],
+          row[11],
+          row[12],
+          row[13],
+          row[14],
+          row[15]])
+      else
+        skip_first_line = false
+      end
+    end
+    @games
+  end
 end
