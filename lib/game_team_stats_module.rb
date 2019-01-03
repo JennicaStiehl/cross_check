1f16f9418d3f9476adbc496078a41f0a3d2e717e
earl-stephens <stephensearl@hotmail.com>
2019-01-03 00:18:51 -0700

    refactored worst_defense to pass spec harness

--- lib/game_team_stats_module.rb
+++ lib/game_team_stats_module.rb
@@ -147,7 +147,6 @@ module GameTeamStats
     game_teams_variable = @game_teams.group_by do |game_id, game|
       game.game_id
     end
-    # binding.pry
     game_teams_variable
   end
 
@@ -157,19 +156,7 @@ module GameTeamStats
       create_game_teams_hash[games[0][1].team_id] = 0
       create_game_teams_hash[games[1][1].team_id] = 0
     end
-    # binding.pry
     create_game_teams_hash
-  end
-
-  def create_game_teams_hash_of_number_of_games_played
-    create_game_teams_hash = {}
-    game_teams_variable.each do |game_id, games|
-      # binding.pry
-      create_game_teams_hash[games[0][1].team_id] = 0
-      create_game_teams_hash[games[1][1].team_id] = 0
-    end
-    # binding.pry
-    create_game_teams_hash_of_number_of_games_played
   end
 
   def add_scores_to_game_teams_hash
@@ -178,19 +165,8 @@ module GameTeamStats
       game_teams_hash[games[0][1].team_id] += games[1][1].goals.to_i
       game_teams_hash[games[1][1].team_id] += games[0][1].goals.to_i
     end
-    # binding.pry
     game_teams_hash
   end
-
-  # def add_number_of_games_played_to_game_teams_hash_of_number_of_games_played
-  #   game_teams_hash_of_games_played = create_game_teams_hash_of_number_of_games_played
-  #   game_teams_variable.each do |game_id, games|
-  #     game_teams_hash_of_games_played[games[0][1].team_id] += 1
-  #     game_teams_hash_[games[1][1].team_id] += 1
-  #   end
-  #   binding.pry
-  #   game_teams_hash_of_games_played
-  # end
 
   def best_defense
      minimum = Hash.new
@@ -201,16 +177,12 @@ module GameTeamStats
            games_played += 1
          end
        end
-       # binding.pry
        minimum[teamid] = (goals.to_f / games_played.to_f)
      end
-     # binding.pry
      minimum_team_id = minimum.key(minimum.values.min).to_i
-     # binding.pry
      best_defense_team = ""
      @teams.values.each do |team|
        team_variable = team.teamid
-       # binding.pry
        if team_variable == minimum_team_id.to_s
          best_defense_team = team.teamName
        end
@@ -218,27 +190,21 @@ module GameTeamStats
      best_defense_team
   end
 
-  # def best_defense
-  #    minimum = add_scores_to_game_teams_hash.values.min
-  #    minimum_team_id = add_scores_to_game_teams_hash.key(minimum).to_i
-  #    # binding.pry
-  #    best_defense_team = ""
-  #    @teams.values.each do |team|
-  #      team_variable = team.teamid
-  #      # binding.pry
-  #      if team_variable == minimum_team_id.to_s
-  #        best_defense_team = team.teamName
-  #      end
-  #    end
-  #    best_defense_team
-  # end
-
   def worst_defense
-     maximum = add_scores_to_game_teams_hash.values.max
-     maximum_team_id = add_scores_to_game_teams_hash.key(maximum).to_i
+     maximum = Hash.new
+     add_scores_to_game_teams_hash.map do |teamid, goals|
+       games_played = 0
+       game_teams_variable.each do |game, team|
+         if team[0][1].team_id == teamid || team[1][1].team_id == teamid
+           games_played += 1
+         end
+       end
+       maximum[teamid] = (goals.to_f / games_played.to_f)
+     end
+      maximum_team_id = maximum.key(maximum.values.max).to_i
      worst_defense_team = ""
-     @teams.values.each do |team|
-       if team.teamid == maximum_team_id.to_s
+     @teams.each do |teamid, team|
+       if teamid == maximum_team_id
          worst_defense_team = team.teamName
        end
      end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -565,7 +565,7 @@ class StatTrackerTest < Minitest::Test
   def test_for_worst_defense
     # skip
     stat_tracker = StatTracker.new
-    stat_tracker.parse_teams('./data/sample_team_info.csv')
+    stat_tracker.parse_teams('./data/team_info.csv')
     stat_tracker.parse_game_teams('./data/shorter_sample_game_teams_stats.csv')
 
     assert_equal "Rangers", stat_tracker.worst_defense
